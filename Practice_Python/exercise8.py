365cf19dd78c949757de7f33288487310c92457b
niel-Da <50853606+niel-Da@users.noreply.github.com>
2019-06-10 16:03:32 -0500

    Added exercise8

--- Practice_Python/exercise8.py
+++ Practice_Python/exercise8.py
@@ -1,8 +1,9 @@
 stop = False
-
+#begin a while loop
 while(not stop):
+#play options
     play = ('rock','paper','scissor')
-
+#request user inputs and confirm what user entered
     player1 = input('Enter player1\'s name:')
     result1 = input('enter "rock","paper","scissors"')
     print(player1,' You typed '+ result1)
@@ -10,25 +11,25 @@ while(not stop):
     result2 = input('enter "rock","paper","scissors"')
     
     print(player2, ' You typed '+ result2)
-
+#check if the play results from player1 and player2 meet this conditions or else jump to the else statment
     if result1 == result2:
-        print('It\'s a tie!\n \n')
+        print('GAME OVER. It\'s a tie!\n \n')
     elif result1 == "rock" and result2 == "scissors":
-        print('player1 wins!\n \n')
+        print('GAME OVER. player1 wins!\n \n')
     elif result1 == "scissors" and result2 == "rock":
-        print('player2 wins!\n \n')
+        print('GAME OVER. player2 wins!\n \n')
     elif result1 == "rock" and result2 == "paper":
-        print('player2 wins!\n \n')
+        print('GAME OVER. player2 wins!\n \n')
     elif result1 == "paper" and result2 == "rock":
-        print('player1 wins!\n \n')
+        print('GAME OVER. player1 wins!\n \n')
     elif result1 == "paper" and result2 == "scissors":
-        print('player2 wins!\n \n')
+        print('GAME OVER. player2 wins!\n \n')
     elif result1 == "scissors" and result2 == "paper":
-        print('player1 wins!\n \n')
+        print('GAME OVER. player1 wins!\n \n')
         
     else:
         print('Something went wrong, please check your spelling and try again. \n \n')
-
+#ask user if they want to play again. if 'yes' restart game. if no end game
     print('Would you like to play again:')
     answer = input('Enter "yes" or "no" if you want to play again: ')
     if answer == "yes":
