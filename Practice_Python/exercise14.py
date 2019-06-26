4c4c5e9c544c8b4dd938a523c21664da16e3df20
Daniel <dan.mygithub@gmail.com>
2019-06-26 15:25:48 -0500

    Added exercise14

---
+++ Practice_Python/exercise14.py
@@ -0,0 +1,25 @@
+
+
+
+# __uthor__ = abrahamd
+
+
+#Write a program (function!) that takes a list and returns a new list
+#that contains all the elements of the first list minus all the
+#duplicates.
+
+
+#define the function 
+def list_One():
+    #give the function a list
+    listOne = [12, 23, 34, 53, 31, 12, 15, 45, 65, 4, 5, 9, 39, 15, 65, 45, 31]
+    #create another list from a set of the initial list
+    listTwo = list(set(listOne))
+    print(listTwo)
+    
+#call the function
+list_One()
+
+
+
+
